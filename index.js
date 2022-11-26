6f0cbf74371aa013cdd2a4db5854fe3add652871
khalid-zein <zeinkhalid004@gmail.com>
2022-11-26 16:04:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+// function addingEventListener() {
+// }
+
+// const input = document.getElementById('button');
+
+// function clickAlert() {
+    //   alert('I was clicked!');
+    // }
+    
+    // input.addEventListener('click', clickAlert);
+    
+//  const input = document.getElementById('button');
+//  input.addEventListener('click', function() {
+//       alert('I was clicked!');
+// });
+
+const btn = document.getElementById('button');
+function clickAlert() {
+    alert('I was clicked!');
+  }
+
 function addingEventListener() {
+    btn.addEventListener("click", clickAlert);
 }
+
+addingEventListener();
